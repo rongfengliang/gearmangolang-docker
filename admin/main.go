1699cb75590c888759917d558f66ce59089b7947
dalongrong <1141591465@qq.com>
2019-01-18 13:31:34 +0800

    add admin demo

---
+++ admin/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+
+	"github.com/appscode/g2/gearadmin"
+)
+
+func main() {
+	c, err := net.Dial("tcp", "localhost:4730")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+	admin := gearadmin.NewGearmanAdmin(c)
+	status, _ := admin.Status()
+	datas, _ := json.Marshal(status)
+	//fmt.Printf("%#v\n", status)
+	fmt.Printf("%#v\n", string(datas))
+
+}
